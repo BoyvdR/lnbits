2ea2b9965487fbf4db08a4d9fd63974d64b5dfa1
dni ⚡ <office@dnilabs.com>
2024-02-23 10:11:20 +0100

    chore: remove lnurl wallet and withdraw feature
    this feature is undocumented and the code is very outdated. i don't think it is worth to keep.
    
    looking at the `/lnurlwallet` endpoint for example, it creates a new user and wallet without even checking if the creation of users is allowed

--- lnbits/core/tasks.py
+++ lnbits/core/tasks.py
@@ -5,7 +5,6 @@ import httpx
 from loguru import logger
 
 from lnbits.core.crud import (
-    get_balance_notify,
     get_wallet,
     get_webpush_subscriptions_for_user,
     mark_webhook_sent,
@@ -99,8 +98,7 @@ def register_task_listeners():
 
 async def wait_for_paid_invoices(invoice_paid_queue: asyncio.Queue):
     """
-    This worker dispatches events to all extensions,
-    dispatches webhooks and balance notifys.
+    This worker dispatches events to all extensions and dispatches webhooks.
     """
     while True:
         payment = await invoice_paid_queue.get()
@@ -113,18 +111,7 @@ async def wait_for_paid_invoices(invoice_paid_queue: asyncio.Queue):
         # dispatch webhook
         if payment.webhook and not payment.webhook_status:
             await dispatch_webhook(payment)
-
-        # dispatch balance_notify
-        url = await get_balance_notify(payment.wallet_id)
-        if url:
-            headers = {"User-Agent": settings.user_agent}
-            async with httpx.AsyncClient(headers=headers) as client:
-                try:
-                    r = await client.post(url, timeout=4)
-                    await mark_webhook_sent(payment, r.status_code)
-                except (httpx.ConnectError, httpx.RequestError):
-                    pass
-
+        # dispatch push notification
         await send_payment_push_notification(payment)
 
 
